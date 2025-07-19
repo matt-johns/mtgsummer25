69d4eff3a4b2868fe64a581c36df654cd8ac98f1
matt-johns <matt.johns.th@gmail.com>
2025-07-19 00:20:41 -0400

    initial commit for mtgsl25 react app

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="padding-smaller">
+        <p className="big-text">MAGIC SUMMER LEAGUE '25</p>
+
+        <header className="App-header">
+          <div img>
+            <p className="big-text">Commanders</p>
+            <div class="container">
+              <div><p class="cmd-titles">Goreclaw, Terror of Qal Sisma</p>
+                <div class="goreclaw"></div>
+                <p>Chris | Points: 0</p>
+              </div>
+              <div><p class="cmd-titles">Ivy, Gleeful Spellthief</p>
+                <div class="ivy"></div>
+                <p>Brenden | Points: 0</p>
+              </div>
+              <div><p class="cmd-titles">Karlov of the Ghost Council</p>
+                <a href="https://tappedout.net/mtg-decks/25-06-25-karlov-of-the-ghost-council/?cb=1752887903" target="_blank" rel="noopener noreferrer"><div class="karlov"></div></a>
+                <p>Matt | Points: 0</p>
+              </div>
+              <div><p class="cmd-titles">Mckiniss's Commander</p>
+                <p>Mckinnis | Points: 0</p>
+              </div>
+              <div><p class="cmd-titles">Carlin's Commander</p>
+                <p>Carlin | Points: 0</p>
+              </div>
+              <div><p class="cmd-titles">Kyle's Commander</p>
+                <p>Kyle | Points: 0</p>
+              </div>
+            </div>
+            <p>
+             
+            </p>
+
+          </div>
+
+        </header>
+
+        <p className="big-text"></p>
+        <div className="padding-smaller"></div>
+
+        <header className="App-header2">
+          <p className="big-text">Current Standings</p>
+          <h1>pingas</h1>
+        </header>
+        <p className="big-text">©2025 Pingas Industries LLC</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
